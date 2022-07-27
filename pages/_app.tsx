21d2d2b3b2687454291ec6c60548be7da9fb0276
Norsninja <norsninja@gmail.com>
2022-07-26 20:05:55 -0700

    replaced template language with my own
    replaced title, description, wallet address, and chain ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,21 +5,21 @@ import Head from "next/head";
 import ThirdwebGuideFooter from "../components/GitHubLink";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mumbai;
+const activeChainId = ChainId.Polygon;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Head>
-        <title>thirdweb NFT Drop Minting Customizable Page</title>
+        <title>Derry Gz Summer Birthday Blowout Minting Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's NFT Drop contract and create a customizable NFT Drop minting page"
+          content="Everybody you think is cool is gonna be at Derry Gz Summer Birthday Blowout this Summer going freaking wild. This is your chance to capture that moment and revel in his glory forever, Derry Gz that is. Every DNGY is born from the crucible of endless variation as a magic bean. When you mint this magic fiesta bean, your life at the party with Derry G begins. Little Dingus is waiting for you at his Summer Birthday Blowout, grow your magic bean now and find yourself front-stage at the most fantabulous party of the century! Viva La DINGY!”"
         />
         <meta
           name="keywords"
-          content="Thirdweb, thirdweb NFT drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
+          content="dogs,nft drop,nft,polygon, art "
         />
       </Head>
       <Component {...pageProps} />
